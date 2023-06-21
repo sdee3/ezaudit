83871d64dc5601561f69fd9d7e9b607ed052abb5
Stefan <stefd996@gmail.com>
2023-06-21 17:12:39 +0200

    Fully working version

--- pages/api/audit.ts
+++ pages/api/audit.ts
@@ -2,11 +2,12 @@ import { NextApiHandler } from 'next'
 
 const handler: NextApiHandler = async (req, res) => {
   const response = await fetch(process.env.NEXT_PUBLIC_LIGHTHOUSE_CRAWL_ROUTE, {
-    method: 'POST',
-    body: JSON.stringify({
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
       url: req.body.url,
       email: req.body.email,
-    }),
+    },
   })
 
   const json = await response.json()
